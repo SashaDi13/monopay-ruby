d86027e6918aa0082c9e28e49eeb7507b7475da0
dhodov_sftsrv <dhodov@softserveinc.com>
2024-08-28 22:52:48 +0300

    add advanced invoive

--- lib/monopay-ruby/invoices/simple_invoice.rb
+++ lib/monopay-ruby/invoices/simple_invoice.rb
@@ -1,3 +1,4 @@
+require 'active_support/all'
 require "bigdecimal"
 require "money"
 
@@ -53,12 +54,11 @@ module MonopayRuby
         end
       end
 
-      private
+      protected
 
-      # Request body required for Monobank API
-      #
-      # @return [Hash] request body
-      def request_body
+      # Default params required for request into Monobank
+
+      def default_params
         # TODO: add "ccy" and another missing params
         # TODO: remove nil valued params
         {
@@ -69,7 +69,14 @@ module MonopayRuby
             reference: reference,
             destination: destination
           }
-        }.to_json
+        }
+      end
+
+      # Request body required for Monobank API
+      #
+      # @return [Hash] request body
+      def request_body
+        default_params.to_json
       end
 
       def convert_to_cents(amount)
